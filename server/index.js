7cc55196091dfdc8535e02f79230b08909b72351
Lloyd's DevMachine <lloydmarcelino@gmail.com>
2023-10-28 18:58:27 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Enable CORS for all routes
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/pokemon/ditto', async (req, res) => {
+  try {
+    // Make a request to the Star Wars API and return the result
+   
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
